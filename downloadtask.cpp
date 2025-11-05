53f0dc8462d9070f2c98714665f8e618935b35ea
shizhuo <m5201314202201@163.com>
2025-11-05 09:17:35 +0800

    download firstmake

--- downloadtask.cpp
+++ downloadtask.cpp
@@ -1,3 +1,160 @@
 #include "downloadtask.h"
 
-DownloadTask::DownloadTask() {}
+DownloadTask::DownloadTask(QObject* parent): QObject(parent) {}
+
+DownloadTask::DownloadTask(const QString &url, const QString &savePath, const QString &fileName): m_url(url), m_savePath(savePath), m_fileName(fileName) {
+
+}
+
+DownloadTask::~DownloadTask() {
+    if (m_reply) {
+        m_reply->deleteLater();
+    }
+    if (m_manager) {
+        m_manager->deleteLater();
+    }
+}
+
+void DownloadTask::startDownload() {
+    qDebug() << "运行线程:" << QThread::currentThreadId();
+    if (m_status == nullptr) {
+        return;
+    }
+    if (m_manager == nullptr) {
+        m_manager = new QNetworkAccessManager(this);
+    }
+    //设置状态
+    setStatus(TaskStatus::Loading);
+    m_paused = false;
+    //请求网络
+    QNetworkRequest request;
+    request.setUrl(QUrl(m_url));
+    //设置http请求头
+    request.setRawHeader(QByteArray("Range"), QString("bytes=" + QString::number(m_bufferByte) + "-").toLocal8Bit());
+    m_reply = m_manager->get(request);
+    qDebug() << "音乐" + m_fileName + "开始下载";
+    //准备写入文件
+    QString fullPath = m_savePath + "/" + m_fileName;
+    QFileInfo fileInfo(fullPath);
+    QDir dir = fileInfo.absoluteDir();
+    if (!dir.exists()) {
+        dir.mkpath(".");
+    }
+    m_file.setFileName(fullPath);
+    if (!m_file.open(QIODevice::WriteOnly) || QIODevice::Append) {
+        m_reply->abort();
+        return;
+    }
+
+    //连接下载完成信号
+    connect(m_reply, &QNetworkReply::finished, this, &DownloadTask::onFinished, Qt::UniqueConnection);
+    //有新数据
+    connect(m_reply, &QNetworkReply::readyRead, this, &DownloadTask::onDownloadReadyRead, Qt::UniqueConnection);
+    //下载进度
+    connect(m_reply, &QNetworkReply::downloadProgress, this, &DownloadTask::onDownloadProgress, Qt::UniqueConnection);
+}
+
+void DownloadTask::pauseDownload() {
+    if (m_status != TaskStatus::Loading) {
+        return;
+    }
+    m_paused = true;
+    setStatus(TaskStatus::Paused);
+    if (m_reply) {
+        m_reply->abort();
+    }
+    qDebug() << "暂停下载" << m_fileName << "已下载" << m_progressByte + m_bufferByte;
+}
+
+void DownloadTask::cancelDownload() {
+    setStatus(TaskStatus::Cancel);
+    if (m_reply) {
+        m_reply->abort();
+    }
+    qDebug() << "取消下载音乐" << m_fileName;
+}
+
+void DownloadTask::onDownloadReadyRead() {
+    QByteArray data = m_reply->readAll();
+    m_progressByte += data.length();
+    m_file.write(data);//写入文件
+}
+
+void DownloadTask::onDownloadProgress(qint64 bytesReceived, qint64 bytesTotal) {
+    if (bytesTotal > 0) {
+        double progress = (double)(bytesReceived + m_bufferByte) / (bytesTotal + m_bufferByte);
+        setProgressValue(progress);
+    }
+}
+
+double DownloadTask::progressValue()const {
+    return m_progressValue;
+}
+
+void DownloadTask::setProgressValue(double newProgressValue) {
+    if (qFuzzyCompare(m_progressByte, newProgressValue)) { //浮点数模糊比较
+        return;
+    }
+
+    m_progressValue = newProgressValue;
+    emit progressValueChanged(m_progressValue);
+}
+
+DownloadTask::TaskStatus DownloadTask::status()const {
+    return m_status;
+}
+
+void DownloadTask::setStatus(TaskStatus newStatus) {
+    if (m_status == newStatus) {
+        return;
+    }
+    m_status = newStatus;
+    emit statusChanged(m_status);
+}
+
+QString DownloadTask::url()const {
+    return m_url;
+}
+
+void DownloadTask::setUrl(const QString &newUrl) {
+    if (m_url == newUrl) {
+        return;
+    }
+    m_url = newUrl;
+    emit urlChanged(m_url);
+}
+
+QString DownloadTask::fileName()const {
+    return m_fileName;
+}
+
+void DownloadTask::setFileName(const QString &newfileName) {
+    if (m_fileName == newfileName) {
+        return;
+    }
+    m_fileName = newfileName;
+    emit fileNameChanged(m_fileName);
+}
+
+QString DownloadTask::savePath()const {
+    return m_savePath;
+}
+
+void DownloadTask::setSavePath(const QString &newSavePath) {
+    if (m_savePath == newSavePath) {
+        return;
+    }
+    m_savePath = newSavePath;
+    emit savePathChanged(m_savePath);
+}
+
+void DownloadTask::onFinished() {
+    m_file.close();
+    auto noError_func = [&]() {
+        setStatus(TaskStatus::Ready);
+        QString suffix = m_url.sliced(m_url.lastIndexOf('.', -1)); //后缀
+        QFileInfo fileInfo(m_file);
+        QString newFileName = fileInfo.absoluteFilePath() + suffix;
+
+    }
+}
